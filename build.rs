ca9937c22d1fc6fb6f2a486669c3dc0965464f66
orafaelmiguel <rafaelmiguel0202@gmail.com>
2025-08-04 03:40:21 -0300

    fix(ci): apply automated code formatting

--- build.rs
+++ build.rs
@@ -5,7 +5,10 @@ fn main() {
 
     if let Ok(kernel_dir) = env::var("KERNEL_DIR") {
         println!("cargo:rerun-if-changed={}/.config", kernel_dir);
-        println!("cargo:warning=Kernux compiling with KERNEL_DIR={}", kernel_dir);
+        println!(
+            "cargo:warning=Kernux compiling with KERNEL_DIR={}",
+            kernel_dir
+        );
     } else {
         println!("cargo:warning=KERNEL_DIR environment variable not set. Build may be incomplete for kernel modules.");
     }
