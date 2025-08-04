afec997f155b8cb18b551fe8cf27b03ede3df364
orafaelmiguel <rafaelmiguel0202@gmail.com>
2025-08-04 03:12:13 -0300

    build: add build.rs script for kernel integration

---
+++ build.rs
@@ -0,0 +1,12 @@
+use std::env;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    if let Ok(kernel_dir) = env::var("KERNEL_DIR") {
+        println!("cargo:rerun-if-changed={}/.config", kernel_dir);
+        println!("cargo:warning=Kernux compiling with KERNEL_DIR={}", kernel_dir);
+    } else {
+        println!("cargo:warning=KERNEL_DIR environment variable not set. Build may be incomplete for kernel modules.");
+    }
+}
